9d84f692937734ccf1d7a68a49cbd41763a94ba3
Daniel Wolter <wolter.daniel@gmail.com>
2024-03-17 18:56:00 -0300

    banco de dados conectado

--- base-backend/bin/base_backend.dart
+++ base-backend/bin/base_backend.dart
@@ -1,8 +1,20 @@
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
+import 'package:mysql1/mysql1.dart';
 
-// import 'package:base_backend/base_backend.dart' as base_backend;
+void main(List<String> arguments) async{
+  final conn = await MySqlConnection.connect(
+    ConnectionSettings(
+      host: 'localhost',
+      port: 3306,
+      user: 'dart_user',
+      db: 'delivery',
+      password: 'dart_pass',
+    ),
+  );
 
-void main(List<String> arguments) {
-  serve((Request req) => Response(200, body: 'Ola mundo', headers: {'content-type': 'application/json'}), 'localhost', 8080,);
+  await conn.query("insert into tb_permissions(name, status) values ('ADMIN', 'A')");
+  print(await conn.query('select * from tb_permissions'));
+
+  await serve((Request req) => Response(200, body: 'Ola mundo', headers: {'content-type': 'application/json'}), 'localhost', 8080,);
 }
