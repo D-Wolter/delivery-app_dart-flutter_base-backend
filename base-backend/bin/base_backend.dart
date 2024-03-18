83b6967efee1dddc393c8e3b80741134f740718d
Daniel Wolter <wolter.daniel@gmail.com>
2024-03-18 13:03:25 -0300

    ultilitario injecao dependencia e leitura .env,

--- base-backend/bin/base_backend.dart
+++ base-backend/bin/base_backend.dart
@@ -2,19 +2,32 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:mysql1/mysql1.dart';
 
-void main(List<String> arguments) async{
-  final conn = await MySqlConnection.connect(
-    ConnectionSettings(
-      host: 'localhost',
-      port: 3306,
-      user: 'dart_user',
-      db: 'delivery',
-      password: 'dart_pass',
-    ),
-  );
+import 'package:commons_core/commons_core.dart';
 
-  await conn.query("insert into tb_permissions(name, status) values ('ADMIN', 'A')");
-  print(await conn.query('select * from tb_permissions'));
+DependencyInjector di = DependencyInjector();
 
-  await serve((Request req) => Response(200, body: 'Ola mundo', headers: {'content-type': 'application/json'}), 'localhost', 8080,);
+void main(List<String> arguments) async {
+  var result = await CustomEnv.get<String>(key: 'chave');
+  print(result);
+
+  // final conn = await MySqlConnection.connect(
+  //   ConnectionSettings(
+  //     host: 'localhost',
+  //     port: 3306,
+  //     user: 'dart_user',
+  //     db: 'delivery',
+  //     password: 'dart_pass',
+  //   ),
+  // );
+
+  // await conn
+  //     .query("insert into tb_permissions(name, status) values ('ADMIN', 'A')");
+  // print(await conn.query('select * from tb_permissions'));
+
+  // await serve(
+  //   (Request req) => Response(200,
+  //       body: 'Ola mundo', headers: {'content-type': 'application/json'}),
+  //   'localhost',
+  //   8080,
+  // );
 }
