3b95380c6bf1a20f99817c0f1df90419cd974505
Daniel Wolter <wolter.daniel@gmail.com>
2024-03-18 16:16:50 -0300

    criado .enc contrato e e banco de dados ok

--- base-backend/bin/base_backend.dart
+++ base-backend/bin/base_backend.dart
@@ -4,25 +4,14 @@ import 'package:mysql1/mysql1.dart';
 
 import 'package:commons_core/commons_core.dart';
 
+import 'core/database/database_mysql_adpter.dart';
+
 DependencyInjector di = DependencyInjector();
 
 void main(List<String> arguments) async {
-  var result = await CustomEnv.get<String>(key: 'chave');
-  print(result);
-
-  // final conn = await MySqlConnection.connect(
-  //   ConnectionSettings(
-  //     host: 'localhost',
-  //     port: 3306,
-  //     user: 'dart_user',
-  //     db: 'delivery',
-  //     password: 'dart_pass',
-  //   ),
-  // );
-
-  // await conn
+  // await DatabaseMySqlAdpter()
   //     .query("insert into tb_permissions(name, status) values ('ADMIN', 'A')");
-  // print(await conn.query('select * from tb_permissions'));
+  print(await DatabaseMySqlAdpter().query('select * from tb_permissions'));
 
   // await serve(
   //   (Request req) => Response(200,
