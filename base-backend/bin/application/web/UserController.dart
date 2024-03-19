80104062f1e40b00d3a243a1230260af2205c679
Daniel Wolter <wolter.daniel@gmail.com>
2024-03-19 11:26:28 -0300

    rota getAll criada

--- base-backend/bin/application/web/UserController.dart
+++ base-backend/bin/application/web/UserController.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
+
 import 'package:shelf/shelf.dart';
 import 'package:shelf/src/middleware.dart';
 import 'package:shelf_router/shelf_router.dart';
 import '../../core/web/controller.dart';
+import '../../domain/models/user.dart';
 import '../../domain/ports/inputs/user_service.dart';
+import '../dtos/user_dto.dart';
 
 class UserController extends Controller {
   final UserService _userService;
@@ -13,7 +17,12 @@ class UserController extends Controller {
   Handler getHandler({List<Middleware>? middlewares, bool isSecurity = false}) {
     final Router router = Router();
 
-    router.get('/users', (Request res) {});
+    router.get('/users', (Request res) async {
+      List<User> users = await _userService.getAllUsers();
+      List<Map> usersJsonMap =
+          users.map((User user) => UserDto.toJson(user)).toList();
+      return Response.ok(jsonEncode(usersJsonMap));
+    });
 
     return createHandler(
       router: router,
